f7feceb9268fb79050bc0f1f5c8ecbbc6964ebb2
Costalesjonatan <42694800+Costalesjonatan@users.noreply.github.com>
2021-02-27 12:24:11 -0300

    Se agrega la estructura basica de la clase de test de vechicleServiceImpl

---
+++ src/test/java/com/costales/practica/ServiceTest/VehicleServiceImplTest.java
@@ -0,0 +1,23 @@
+package com.costales.practica.ServiceTest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VehicleServiceImplTest {
+
+    public void shouldCreateAVechicle(){
+
+    }
+
+    public void shouldDeleteAVechicle(){
+
+    }
+
+    public void shouldGetAVechicleByID(){
+
+    }
+
+    public void ShouldGetAlVechicles(){
+
+    }
+}
